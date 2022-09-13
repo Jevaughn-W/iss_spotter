140178932a53e5b376342d0dc4e56fae7bcf8ab2
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2022-09-12 21:46:05 -0400

    add function to grab flyovertimes based on user coordinates

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -9,6 +9,14 @@ fetchMyIP((error, ip) => {
 });
 
 // fetchCoordsByIP("149.248.184.59", (error, coordinates) => {
+//   if (error) {
+//     console.log("It didnt't work!", error);
+//     return;
+//   }
+//   console.log('It worked! Return IP:', coordinates); // Reminder, it knows its not an error because the variable is a falsy
+// });
+
+// fetchISSFlyOverTimes({ latitude: 43.653226, longitude: -79.3831843 }, (error, coordinates) => {
 //   if (error) {
 //     console.log("It didnt't work!", error);
 //     return;
--- iss.js
+++ iss.js
@@ -18,28 +18,47 @@ const fetchMyIP = function(callback) {
 };
 // fetchMyIP(input => console.log(input)); // test to ensure function works
 
-const fetchCoordsByIP = function (ip, callback) {
-  request("http://ipwho.is/" + ip, (err, request, body) => {
-  if (err) {
-    callback(err, null)
-    return;
+const fetchCoordsByIP = function(ip, callback) {
+  request("http://ipwho.is/" + ip, (err, response, body) => {
+    if (err) {
+      callback(err, null);
+      return;
     }
-  if (request.statusCode !== 200) {
-    const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
-    callback(Error(msg, null));
-    return;   
-  }
-  if (!JSON.parse(body).success) {
-    callback(`Error: Success status was false. Server message says: Invalid IP address when fetching for IP ${ip}.`, null);
+    if (request.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching Coords. Response: ${body}`;
+      callback(msg, null);
+      return;
+    }
+    if (!JSON.parse(body).success) {
+      callback(`Error: Success status was false. Server message says: Invalid IP address when fetching for IP ${ip}.`, null);
+      return;
+    }
+    let coord = {};
+    coord.latitude =  JSON.parse(body).latitude;
+    coord.longitude =  JSON.parse(body).longitude;
+    callback(err, coord);
     return;
-  }
-  let coord = {};
-  coord.latitude =  JSON.parse(body).latitude;
-  coord.longitude =  JSON.parse(body).longitude;
-  callback(err, coord);
-  return;
-
   });
 };
 
-module.exports = { fetchMyIP, fetchCoordsByIP };
+const fetchISSFlyOverTimes = function(coords, callback) {
+  request(`https://iss-pass.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`, (err, response, body) => {
+    if (err) {
+      callback(err, null);
+      return;
+    }
+    if (request.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching ISS flyover times. Response: ${body}`;
+      callback(msg, null);
+      return;
+    }
+    let result = JSON.parse(body);
+    if (result.message !== "success") {
+      callback(`Error: ${result} fetching ISS flyover time. Response: ${body}`, null);
+    }
+    callback(err, result.response);
+    return;
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
