093b489074d6638d53a0be9e757816eb4d4e1130
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2022-09-12 20:59:51 -0400

    removed coord check

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ fetchMyIP((error, ip) => {
   console.log('It worked! Return IP:', ip); // Reminder, it knows its not an error because the variable is a falsy
 });
 
-fetchCoordsByIP("149.2d48.184.59", (error, coordinates) => {
-  if (error) {
-    console.log("It didnt't work!", error);
-    return;
-  }
-  console.log('It worked! Return IP:', coordinates); // Reminder, it knows its not an error because the variable is a falsy
-});
+// fetchCoordsByIP("149.248.184.59", (error, coordinates) => {
+//   if (error) {
+//     console.log("It didnt't work!", error);
+//     return;
+//   }
+//   console.log('It worked! Return IP:', coordinates); // Reminder, it knows its not an error because the variable is a falsy
+// });
